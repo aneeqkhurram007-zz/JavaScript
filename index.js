5b26379b120cb8c662b1f67eb69478d2bfddaffc
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-09-18 01:10:57 +0500

    1.10

---
+++ index.js
@@ -0,0 +1,12 @@
+console.time("mycode");
+console.log("Hello Console");
+console.log(34 + 4);
+console.log(true);
+console.log([34, 2, 1, 2]);
+console.table({ harry: "this", marks: 34 });
+console.warn("This is a warning");
+console.log({ harry: "Aneeq", age: 234 });
+//console.clear();
+console.timeEnd("mycode");
+console.assert(234 < 189, "This is possible");
+console.error("This is an error");
---
+++ new.js
@@ -0,0 +1,5 @@
+var x = 5;
+var y = 6;
+document.write("Thuis is als");
+console.log(y * 5);
+alert("THis is a");
