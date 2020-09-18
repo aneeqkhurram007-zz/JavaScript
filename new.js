548b05d558054097776f0c0ab4070345032cef9d
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2020-09-18 14:07:11 +0500

    12

--- new.js
+++ new.js
@@ -1,5 +1,40 @@
-var x = 5;
-var y = 6;
-document.write("Thuis is als");
-console.log(y * 5);
-alert("THis is a");
+//premitive Data Tyoes
+var name = "harry";
+var channel = 0;
+const nameO = "Cannot Do Anything";
+console.log(name, channel, nameO);
+//Scope
+
+{
+  let name = "Sehikh";
+}
+console.log(typeof channel);
+let isDrive = true;
+let nullVar = null;
+console.log(typeof isDrive);
+let undef = undefined;
+console.log(typeof undef);
+//Object Literals
+let marks = {
+  harry: 34,
+  aneeq: "50",
+  sheikh: 64,
+};
+console.log(marks);
+function name(params) {}
+//type Conversion and Coercion
+let myVar;
+myVar = String(34);
+let booleanVar = true;
+booleanVar = String(false);
+console.log("Data type is " + typeof booleanVar + " " + booleanVar);
+let date = new Date();
+console.log(date + typeof date);
+let array = [1, 2, 3, 4];
+console.log(array.length, typeof array);
+let newNumber = parseFloat("354.3548");
+console.log(newNumber.toFixed(2), typeof newNumber);
+let html = "this is a heading";
+console.log(html.substring(1, 6));
+console.log(html.includes("heading"));
+console.log(html.endsWith("headinda"));
