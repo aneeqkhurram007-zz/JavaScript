c1fd686cd3585a4f88c7f7e3fbd412e1733aaa23
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-08-18 15:29:54 +0500

    project 2

--- Projects/Project 2/project2.js
+++ Projects/Project 2/project2.js
@@ -1,17 +1,31 @@
 const jokePara = document.getElementById('jokeText');
 const jokeButton = document.querySelector('#jokeButton')
 
-const generateJokes = () => {
+const generateJokes = async () => {
     const setHeader = {
         headers: {
             Accept: "application/json"
         }
     }
-    fetch('https://icanhazdadjoke.com', setHeader)
-        .then((res) => res.json()
-        ).then((data) => jokePara.innerHTML = data.joke).catch((error) => {
-            console.log(error);
-        })
+    // without async programming
+    // fetch('https://icanhazdadjoke.com', setHeader)
+    //     .then((res) => res.json()
+    //     ).then((data) => jokePara.innerHTML = data.joke).catch((error) => {
+    //         console.log(error);
+    //     })
+
+
+    // with async programming (Test 🏳️)
+    // const res = await (await fetch("https://restcountries.eu/rest/v2/name/nepal")).json()
+    // console.log(res);
+    // const [data] = res;
+    // console.log(data);
+    // jokePara.innerHTML = data.name
+
+    const res = await fetch("https://icanhazdadjoke.com", setHeader);
+    const data = await res.json();
+    jokePara.innerHTML = data.joke
+
 }
 
 jokeButton.addEventListener("click", generateJokes);
