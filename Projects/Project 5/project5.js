a0e367bc1179fe058a27f392c75ab8b538ed10cb
aneeqkhurram007 <aneeqkhurram007@yahoo.com>
2021-08-20 10:09:57 +0500

    10.09

--- Projects/Project 5/project5.js
+++ Projects/Project 5/project5.js
@@ -2,6 +2,20 @@ document.querySelectorAll("h1").forEach((ele) => ele.className = "counter")
 const counters = document.querySelectorAll(".counter")
 counters.forEach((counter) => {
     counter.innerHTML = 0
+
+    //1st Method
+    // setInterval(() => {
+    //     const targetCount = +counter.getAttribute('data-target');
+    //     // console.log(typeof (targetCount));
+    //     const startingCount = +counter.innerHTML
+    //     const incr = targetCount / 100;
+    //     if (startingCount < targetCount) {
+    //         counter.innerHTML = `${startingCount + incr}`
+    //         // setTimeout(updatedCounter, 10)
+    //     }
+
+    // }, 10);
+
     const updatedCounter = () => {
         const targetCount = +counter.getAttribute('data-target');
         // console.log(typeof (targetCount));
