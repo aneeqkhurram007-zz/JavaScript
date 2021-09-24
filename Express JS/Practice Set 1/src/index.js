4d4254b7efe2884bb1a89ee9cdb08c0637638a5a
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-09-24 12:31:28 +0500

    Practice Set 1

---
+++ Express JS/Practice Set 1/src/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 8000
+
+app.set('view engine', 'hbs')
+app.get('/', (req, res) => {
+    res.render('index')
+})
+
+app.listen(port, () => {
+    console.log("Listening on port " + port)
+})
