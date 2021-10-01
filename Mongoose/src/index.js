edca690e4c003b0e17fa3d9bff216e42f6ff7c3e
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-10-01 15:55:16 +0500

    partials added

--- Mongoose/src/index.js
+++ Mongoose/src/index.js
@@ -6,10 +6,13 @@ const port = 8000
 
 const template = path.join(__dirname, '../template/views')
 app.use(express.static(path.join(__dirname, '../public')))
+hbs.registerPartials(path.join(__dirname, '../template/partials'))
 app.set('view engine', 'hbs')
 app.set('views', template)
 app.get('/', (req, res) => {
-    res.render('index')
+    res.render('index', {
+        name: "Aneeq Khurram"
+    })
 })
 
 app.listen(port)
