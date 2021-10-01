5ea8110a9426f03d1e6c307d82159d0679572306
Aneeq Khurram <aneeqkhurram007@yahoo.com>
2021-10-01 16:32:59 +0500

    save method

--- Mongoose/src/index.js
+++ Mongoose/src/index.js
@@ -3,7 +3,45 @@ const app = express()
 const path = require('path')
 const hbs = require('hbs')
 const port = 8000
+const mongoose = require('mongoose')
+//Database
+mongoose.connect('mongodb://localhost:27017/testDB')
+// Schema -- Structure of Document
+const playlistSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    ctype: String,
+    videos: Number,
+    author: String,
+    active: Boolean,
+    date: {
+        type: Date,
+        default: Date.now()
+    }
+})
+//Model -- For Creating Collection
+const Playlist = new mongoose.model("Playlist", playlistSchema)
+//Creating a document 
+const createDocument = async () => {
+    try {
+        const playlist = new Playlist({
+            name: "Node JS",
+            ctype: "Back-End",
+            videos: 2,
+            author: "Windows 11",
+            active: true,
+        })
+        const result = await playlist.save()
+        console.log(result);
 
+    } catch (error) {
+        console.log(error);
+    }
+}
+createDocument()
+//Server
 const template = path.join(__dirname, '../template/views')
 app.use(express.static(path.join(__dirname, '../public')))
 hbs.registerPartials(path.join(__dirname, '../template/partials'))
